6ca67d371db8c1341ecde5133475cacdabd23a64
Yanis <yanis.yzr@gmail.com>
2023-10-06 17:44:13 +0200

    debut lecteur_csv.h

---
+++ src/CSV/lecture_csv.h
@@ -0,0 +1,14 @@
+/*Paramètre :*/
+/*lecture_csv(../fichier/*.csv (voir sr2 aussi)*/
+/*FILE * fich = NULL
+    fopen...*/
+/* ========================================== */
+/*inserer(____, mot)*/
+/*BALISES DOXYGEN*/
+
+
+#include <stdio.h>
+
+
+char** recherche_mot(char * mot, FILE csv); //regarder config.c sr2
+char** recherche_indice(int i_deb, int i_fin, int j_deb, int j_fin);
