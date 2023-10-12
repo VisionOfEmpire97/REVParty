7aacc3a1f8796c30b841b90a9c95640213f7e626
Remy Joseph <93032635+AxL-J-8@users.noreply.github.com>
2023-10-12 23:34:05 +0200

    strcuture matrice de chaine dynamique
    le fichier .c et .h des structures matrice de chaine avec un fichier test

---
+++ src/utils_sd/matrice/test/test_matrice.c
@@ -0,0 +1,21 @@
+#include "../code/matrice.h"
+/// \brief program de teste sur une matrice
+
+int main(void)
+{
+    /// creation de la matrice
+    t_mat_char_star_dyn *ma_matrice = creer_matrice(1, 2);
+
+    /// insertion des valeurs dans la matrice
+    inserer_matrice_char("1", ma_matrice);
+
+    inserer_matrice_char("2", ma_matrice);
+
+    inserer_matrice_char("3", ma_matrice);
+
+    /// affichage de la matrice
+    afficher_matrice_char(ma_matrice);
+    /// suppression de la matrice
+    supprimer_matrice_char(ma_matrice);
+    return 0;
+}
