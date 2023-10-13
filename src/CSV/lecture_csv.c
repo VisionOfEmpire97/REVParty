0397ab229f1997751e81f01da4bfbe8867b417e2
NpiePrime <yanis.yzr@gmail.com>
2023-10-13 19:30:00 +0200

    resolution probleme de type

--- src/CSV/lecture_csv.c
+++ src/CSV/lecture_csv.c
@@ -8,35 +8,48 @@
 #include <unistd.h>
 #include <string.h>
 #include "lecture_csv.h"
+#include "../utils_sd/matrice/code/matrice.h"
 
 #define N 10
 #define SEPARATEUR ","
 
-char data[N];
+char* data;
 
 
-t_mat_char_star_dyn *lecture_par_ligne(char buffer[],FILE* file ){
-t_mat_char_star_dyn *mat = creer_matrice(5, 5);
-    fgets(buffer, sizeof(buffer), file);
-    printf("%s", buffer);
+t_mat_char_star_dyn *lecture_par_ligne(char* buffer,FILE* file ){
+
+    void *mat = creer_matrice(1, 6);
+
+    fgets(buffer, sizeof(char*), file);
+    
+    printf("%s\n", buffer);
+    
+    
     char* tok = strtok(buffer, SEPARATEUR);
-    tok = strcat(tok,strtok(NULL, SEPARATEUR));
-    tok = strcat(tok,strtok(NULL, SEPARATEUR));
-    tok = strcat(tok,strtok(NULL, SEPARATEUR));
-    tok = strcat(tok,strtok(NULL, SEPARATEUR));
-    sscanf(tok, "%s", data); 
-    printf("%s\n", data);
+    inserer_matrice_char(tok, mat);
+    tok = strtok(NULL, SEPARATEUR);
+    inserer_matrice_char(tok, mat);
+    tok = strtok(NULL, SEPARATEUR);
+    inserer_matrice_char(tok, mat);
+    tok = strtok(NULL, SEPARATEUR);
+    inserer_matrice_char(tok, mat);
+    // tok = strtok(NULL, SEPARATEUR);
+    // inserer_matrice_char(tok, mat);
+
+    printf("test\n");
+    afficher_matrice_char(mat);
+   
     return mat;
 }
 
 t_mat_char_star_dyn *lecture_par_col(char buffer[], FILE* file){
-    t_mat_char_star_dyn *mat = creer_matrice(5, 5);
-    while (fgets(buffer, sizeof(buffer), file)!=NULL)
+    t_mat_char_star_dyn *mat = creer_matrice(6, 1);
+    while (fgets(buffer, sizeof(char*), file)!=NULL)
     {
-        printf("%s\n", buffer);
         char* tok = strtok(buffer, SEPARATEUR);
-        sscanf(tok, "%s", data); 
-        printf("%s\n", data);
+        printf("%s\n", tok);
+        inserer_matrice_char(tok, mat);
+        afficher_matrice_char(mat);
     }
     return mat;
 
@@ -61,19 +74,23 @@ int main(int argc, char const *argv[])
     }
 
     char buffer[100];
-
-    
-
-        
-    // printf("%s",lecture_par_ligne(buffer, file));
-
-
-
-    // sscanf(tok, "%s", data); 
-    // printf("%s\n", data); 
-    
+    lecture_par_ligne(buffer, file);
+    printf("===================================================\n");
     // lecture_par_col(buffer, file);
-    
+    while (fgets(buffer, sizeof(buffer), file)!=NULL)
+    {
+        char* tok = strtok(buffer, ",");
+
+        int data;
+        sscanf(tok, "%d", &data);
+        tok = strtok(NULL, ","); //On passe à la colonne suivante
+        tok = strtok(NULL, ",");
+        tok = strtok(NULL, ",");
+        // tok = strtok(NULL, ",");
+        printf("%s\n", tok);
+        printf("\n");
+        printf("%d\n", data);
+    }
     fclose(file);
     
 
