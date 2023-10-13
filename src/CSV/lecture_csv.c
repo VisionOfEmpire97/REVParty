4b3ab5466d531115ab1ee1bae93ce4c1852873c0
NpiePrime <yanis.yzr@gmail.com>
2023-10-13 16:19:10 +0200

    typage

--- src/CSV/lecture_csv.c
+++ src/CSV/lecture_csv.c
@@ -60,50 +60,9 @@ int main(int argc, char const *argv[])
 
     char buffer[100];
 
-    // while (fgets(buffer, sizeof(buffer), file)!=NULL)
-    // {
-    //     // printf("%s\n", buffer);
-    //     char* tok = strtok(buffer, SEPARATEUR);
-    //     sscanf(tok, "%s", data); 
-    //     printf("%s\n", data);
-
-        // char* tok2 = strtok(NULL, SEPARATEUR);
-        // sscanf(tok2, "%s", data2);
-        // // printf("%s\n", data2);
-
-        // char* tok3 = strtok(NULL, SEPARATEUR);
-        // sscanf(tok3, "%s", data3);
-        // // printf("%s\n", data3);
-
-        // char* tok4 = strtok(NULL, SEPARATEUR);
-        // sscanf(tok4, "%s", data4);
-        // // printf("%s\n", data4);
-
-        // char* tok5 = strtok(NULL, SEPARATEUR);
-        // sscanf(tok5, "%s", data5);
-        // // printf("%s\n", data5);
+    
 
         
-
-    // }
-    // fgets(buffer, sizeof(buffer), file);
-    // printf("%s", buffer);
-    // char* tok = strtok(buffer, SEPARATEUR);
-    // tok = strcat(tok,strtok(NULL, SEPARATEUR));
-    // tok = strcat(tok,strtok(NULL, SEPARATEUR));
-    // tok = strcat(tok,strtok(NULL, SEPARATEUR));
-    // tok = strcat(tok,strtok(NULL, SEPARATEUR));
-    // sscanf(tok, "%s", data); 
-    // printf("%s\n", data);
-
-    // fgets(buffer, sizeof(buffer), file);
-    // printf("%s", buffer);
-    // tok = strtok(buffer, SEPARATEUR);
-    // tok = strcat(tok,strtok(NULL, SEPARATEUR));
-    // tok = strcat(tok,strtok(NULL, SEPARATEUR));
-    // tok = strcat(tok,strtok(NULL, SEPARATEUR));
-    // tok = strcat(tok,strtok(NULL, SEPARATEUR));
-
     // printf("%s",lecture_par_ligne(buffer, file));
 
 
