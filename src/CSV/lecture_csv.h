d16268cca11f2041a31ef675e2f21f2bc5009929
Yanis <yanis.yzr@gmail.com>
2023-10-13 16:08:27 +0200

    probleme de typage csv

--- src/CSV/lecture_csv.h
+++ src/CSV/lecture_csv.h
@@ -10,5 +10,8 @@
 #include <stdio.h>
 
 
+char** lecture_par_ligne(char buffer[],FILE* file );
+char** lecture_par_col(char buffer[], FILE* file);
+
 char** recherche_mot(char * mot, FILE csv); //regarder config.c sr2
 char** recherche_indice(int i_deb, int i_fin, int j_deb, int j_fin);
