5ca7c06de291261bcc3ada43ea28716f24a41148
NpiePrime <yanis.yzr@gmail.com>
2023-10-16 11:48:47 +0200

    matrice purement dynamique

--- src/utils_sd/matrice/matrice.c
+++ src/utils_sd/matrice/matrice.c
@@ -10,21 +10,14 @@ typedef struct t_mat_char
     char ***matrice;
 } t_mat_char_star_dyn;
 
-t_mat_char_star_dyn *creer_matrice(int ligne, int colonne)
+t_mat_char_star_dyn *creer_matrice()
 {
     t_mat_char_star_dyn *m = malloc(sizeof(struct t_mat_char));
-    m->ligne = ligne;
-    m->col = colonne;
+    m->ligne = 0;
+    m->col = 0;
     m->nb_elem = 0;
-    m->matrice = (char ***)malloc(ligne * sizeof(char **));
-    for (int i = 0; i < ligne; i++)
-    {
-        m->matrice[i] = (char **)malloc(colonne * sizeof(char *));
-        for (int j = 0; j < colonne; j++)
-        {
-            m->matrice[i][j] = "";
-        }
-    }
+    m->matrice = (char ***)malloc(sizeof(char **));
+
     return m;
 }
 
@@ -33,6 +26,13 @@ t_mat_char_star_dyn *inserer_matrice_char(char *chaine, t_mat_char_star_dyn *mat
     int no = mat->nb_elem;
     int ligne = mat->ligne;
     int colonne = mat->col;
+   
+    if(ligne==1){
+         mat->col++;
+        realloc(mat->matrice[0],colonne * sizeof(char *));
+
+    }
+
     if (no < (ligne) * (colonne))
     {
         // recuperer la case en fonction du nombre d'element
@@ -40,7 +40,9 @@ t_mat_char_star_dyn *inserer_matrice_char(char *chaine, t_mat_char_star_dyn *mat
         int c = no % colonne;
         mat->matrice[l][c] = chaine;
         (mat->nb_elem)++;
-    }
+    }      
+    
+
     return mat;
 }
 
@@ -70,4 +72,9 @@ void supprimer_matrice_char(t_mat_char_star_dyn *mat)
     }
     free(M);
     free(mat);
+}
+t_mat_char_star_dyn *ajouter_ligne(t_mat_char_star_dyn *mat){
+    mat->ligne++;
+    realloc(mat->matrice,(mat->ligne* sizeof(char **)));
+    return mat;
 }
