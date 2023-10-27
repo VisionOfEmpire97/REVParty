c46b7949b2590ef810a13d981913fe8f35b1affb
Remy Joseph <93032635+AxL-J-8@users.noreply.github.com>
2023-10-27 17:35:19 +0200

    documentation matrice.h

--- src/utils_sd/matrice/matrice.h
+++ src/utils_sd/matrice/matrice.h
@@ -1,3 +1,8 @@
+/**
+ * \author Equipe 001 (JOSEPH REMY)
+ * \date 16 Octobre 2023
+ * \brief Fichier de definition de la structure de donnée
+ */
 #ifndef __MATRICE_H__
 #define __MATRICE_H__
 #include "stdbool.h"
@@ -6,13 +11,13 @@
 
 /** \defgroup ADTMatrice Matrice
  Documentation de l'implémentation de la structure de données Matrice
- @{
+ *@{
  *
  */
 
 /**
  * \defgroup Type Type de données Matrice de chaines
- * @{
+ *@{
  * \brief Structure de donnee matrice.
  * \struct t_mat_char_star_dyn une structure de matrice de chaine dynamique
  */
@@ -21,60 +26,63 @@ typedef struct t_mat_char t_mat_char_star_dyn;
 /**@}*/
 
 /** \defgroup Constructeur Constructeurs et destructeurs de matrice
- * @{
+ *@{
  * \brief Creation d'une matrice vide.
- * \return une matrice de charactere vide
+ * \return Une matrice de charactere vide
  */
 t_mat_char_star_dyn *creer_matrice();
 
 /**
  * \brief Insertion dans une matrice de chaine
- * \param chaine l'element a inserer
- * \param mat la matrice dans laquelle on va inserer l'element
+ * \param chaine L'element a inserer
+ * \param mat La matrice dans laquelle on va inserer l'element
  * \return 0 si la valeur a bien été inséré -1 sinon
  */
 int inserer_matrice_char(char *chaine, t_mat_char_star_dyn *mat);
 
 /**
  * \brief Faire l'affichage d'une matrice sur stdout.
- * \param mat la matrice a afficher
+ * \param mat La matrice a afficher
  */
 void afficher_matrice_char(t_mat_char_star_dyn *mat);
 
 /**
  * \brief Supprime et libere la memoire allouée a la structure
- * \param mat la structure à liberer
+ * \param mat La structure à liberer
  */
 void supprimer_matrice_char(t_mat_char_star_dyn *mat);
 
 /**
  * \brief Ajouter une ligne a la matrice
  * \param mat
- * \return la matrice en entree avec une ligne ajoutée
+ * \return La matrice en entree avec une ligne ajoutée
  */
 
 t_mat_char_star_dyn *ajouter_ligne(t_mat_char_star_dyn *mat);
 
 /**
- * \brief renvoie si la matrice est vide ou non
+ * \brief Renvoie si la matrice est vide ou non
  * \param mat
+ * \return true si la matrice est vide
+ * \return false si il y a au moins un element dans la matrice
  */
 bool est_matrice_vide(t_mat_char_star_dyn *mat);
 
 /**
- * \brief récuperer la valeur a une case precise de la matrice
+ * \brief Récuperer la valeur a une case precise de la matrice
  * \param mat
- * \param ligne la ligne a laquelle on veut accéder
- * \param colonne la colonne a laquelle on veut accéder
- * \return la valeur a la case donnée ou NULL si l'indice est incorrecte
+ * \param ligne La ligne a laquelle on veut accéder
+ * \param colonne La colonne a laquelle on veut accéder
+ * \return La valeur a la case donnée ou NULL si l'indice est incorrecte
  */
 char *valeur_matrice_char_indice(t_mat_char_star_dyn *mat, int ligne, int colonne);
 
 /**
- * \brief récuperer le nombre de colonne de la matrice
+ * \brief Récuperer le nombre de colonne de la matrice
  * \param mat
- * \return le nombre de colonnes de la matrice
+ * \return Le nombre de colonnes de la matrice
  */
 int recuperer_nb_colonnes(t_mat_char_star_dyn *mat);
 /**@}*/
+/**@*/
 #endif
