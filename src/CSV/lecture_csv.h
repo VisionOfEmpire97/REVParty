31f68ec8882fd172187729213402c88103943774
Nathan ROSET <nathroset97@gmail.com>
2023-10-27 03:18:42 +0200

    - Mise en forme des .c - ajout du fichier de configuration Doxyfile (à compléter) - Mise en forme des commentaires Doxygen (à compléter)

--- src/CSV/lecture_csv.h
+++ src/CSV/lecture_csv.h
@@ -1,25 +1,32 @@
-/// \author Equipe 001 (ZERKANI Yanis)
-/// \date 16 Octobre 2023
-/// \brief Programme permettant de lire un fichier CSV et de renvoyer son contenu sous forme de matrice de type t_mat_char_star_dyn
-
+/**
+ * \author Equipe 001 (ZERKANI Yanis)
+ * \date 16 Octobre 2023
+ * \brief Programme permettant de lire un fichier CSV et de renvoyer son contenu sous forme de matrice.
+ * La matrice est de type t_mat_char_star_dyn.
+ */
 #include <stdio.h>
 #include "../utils_sd/matrice/matrice.h"
 
-/// \brief Lecture et traduction en matrice de l'entete du fichier csv
-/// \param[in] file le fichier à parser
-/// \param[out] mat la matrice contenant la premiere ligne du fichier
+/**
+ * \brief Lecture et traduction en matrice de l'entete du fichier csv
+ * \param[in] file le fichier à parser
+ * \param[out] mat la matrice contenant la premiere ligne du fichier
+ */
 t_mat_char_star_dyn *lecture_entete(const char *file);
 
-/// \brief Recherche de la ligne contenant le hash
-/// \param hash la chaine de caractères recherchée
-/// \param file le fichier à parser
-/// \param[out] mat la matrice contenant la ligne dans le fichier (matrice vide si aucune ligne ne correspond)
+/**
+ * \brief Recherche de la ligne contenant le hash
+ * \param hash la chaine de caractères recherchée
+ * \param file le fichier à parser
+ * \param[out] mat la matrice contenant la ligne dans le fichier (matrice vide si aucune ligne ne correspond)
+ */
 t_mat_char_star_dyn *recherche_hash(char *hash, const char *file);
 
-t_mat_char_star_dyn *recherche_indice(int i_deb, int i_fin, int j_deb, int j_fin);
+//t_mat_char_star_dyn *recherche_indice(int i_deb, int i_fin, int j_deb, int j_fin);
 
-/// \brief Permet d'inserer une chaine dans la matrice
-/// \param chaine chaine à insérer
-/// \param mat matrice destination
-
+/**
+ * \brief Permet d'inserer une chaine dans la matrice
+ * \param chaine chaine à insérer
+ * \param mat matrice destination
+ */
 void inserer_ligne_matrice(char *chaine, t_mat_char_star_dyn *mat);
--- src/verif_m_vote.c
+++ src/verif_m_vote.c
@@ -8,62 +8,78 @@
 #include "CSV/lecture_csv.h"
 #include "utils_sd/matrice/matrice.h"
 #define STRLONG 120
-
-void affichage_resultat(t_mat_char_star_dyn *entete ,t_mat_char_star_dyn *result )
-{   
-    char*entete_valeur = NULL;
-    char*result_valeur = NULL;
-    for (int i=4 ; i<recuperer_nb_colonnes(result); i ++ )
-    {   
-        entete_valeur = valeur_matrice_char_indice(entete,0,i);
-        result_valeur = valeur_matrice_char_indice(result,0,i);
-        printf("%-40s%3s \n",entete_valeur,result_valeur);
- 
+/**
+ * @fn formate et affiche le résultat de la recherche
+ * @param entete
+ * @param result
+**/
+void affichage_resultat(t_mat_char_star_dyn *entete, t_mat_char_star_dyn *result)
+{
+    char *entete_valeur = NULL;
+    char *result_valeur = NULL;
+    for (int i = 4; i < recuperer_nb_colonnes(result); i++)
+    {
+        entete_valeur = valeur_matrice_char_indice(entete, 0, i);
+        result_valeur = valeur_matrice_char_indice(result, 0, i);
+        printf("%-40s%3s \n", entete_valeur, result_valeur);
     }
 }
 
-/*Fonction met en majuscule pour le nom */
-/// \brief Fonction verification majuscule pour le nom
-/// \param[in] On récupere le nom
+/** 
+ * Fonction met en majuscule pour le nom 
+ * \brief Fonction verification majuscule pour le nom
+ * \param[in] On récupere le nom
+**/
 
-void Majuscule(char *chaine) {
-    for (int i = 0; chaine[i] != '\0'; i++) {
-        if (islower(chaine[i])) {
+void Majuscule(char *chaine)
+{
+    for (int i = 0; chaine[i] != '\0'; i++)
+    {
+        if (islower(chaine[i]))
+        {
             chaine[i] = toupper(chaine[i]); // Convertir la lettre minuscule en majuscule
         }
     }
 }
 /*Fonction met en minuscule pour le prenom */
 /// \brief Fonction passe en minuscule le prenom et sa première lettre en majuscule
-/// \remarks on commence au deuxieme caractere comme le premier doit etre une majuscule
+/// \remarks on commence au deuxième caractere comme le premier doit être une majuscule
 /// \param[in] On récupere le prenom
 
-void Minuscules(char *chaine) {
-    if (chaine[0] != '\0') {
+void Minuscules(char *chaine)
+{
+    if (chaine[0] != '\0')
+    {
         chaine[0] = toupper(chaine[0]); // Mettre en majuscule la première lettre
     }
 
-    for (int i = 1; chaine[i] != '\0'; i++) {
-        if (isupper(chaine[i])) {
+    for (int i = 1; chaine[i] != '\0'; i++)
+    {
+        if (isupper(chaine[i]))
+        {
             chaine[i] = tolower(chaine[i]); // Convertir les lettres majuscules en minuscules
         }
     }
 }
 
-/* Programme principal - Vérif my vote :)  */
-/// \brief Fonction main verif vote
-/// \author Antoine Vallat
-/// \date 13 octobre 2023
-/// \param[in] On récupere le nom , prenom , le code et le nom du csv dans argv
-
-int main(int argc, char* argv[])
+/** Programme principal - Vérif my vote :) 
+ * \brief Fonction main verif vote
+ * \author Antoine Vallat
+ * \date 13 octobre 2023
+ * \fn On récupere le nom , prenom , le code et le nom du csv dans argv
+ * \param[in] nom
+ * \param[in] prénom
+ * \param[in] CodePersonnel
+ * \param[in] nom_csv
+**/
+int main(int argc, char *argv[])
 {
-    char* nom;
-    char* prenom;
-    char* code;
-    char* nom_csv ;
-    t_mat_char_star_dyn * ligne ;
-    if (argc==5)
+    char *nom;
+    char *prenom;
+    char *code;
+    char *nom_csv;
+    t_mat_char_star_dyn *ligne;
+    if (argc == 5)
     {
         nom = argv[1];
         prenom = argv[2];
@@ -72,7 +88,7 @@ int main(int argc, char* argv[])
     }
     else
     {
-        fprintf(stderr,"Veuillez mettre tout les parametres nom prenom code nom_csv\n");
+        fprintf(stderr, "usage :./bin/verify_my_vote nom prenom code nom_csv\n");
         exit(1);
     }
 
@@ -82,32 +98,27 @@ int main(int argc, char* argv[])
     Majuscule(nom);
     Minuscules(prenom);
 
-
     /* On fait le hash du code  */
-    /// \brief Partie hash du code client
+    /// \brief Partie hash du code personnel
     int bufferSize = SHA256_BLOCK_SIZE;
-    char hashRes[bufferSize*2 + 1]; // contiendra le hash en hexadécimal
-	char * item = malloc(STRLONG*sizeof(char)); // contiendra la chaîne à hasher
-    strcat(item, nom);
-    strcat(item, " ");
-    strcat(item, prenom);
-    strcat(item, code);
+    char hashresult[bufferSize * 2 + 1];            /**< contiendra le hash en hexadécimal */
+    char *stringtohash = malloc(STRLONG * sizeof(char));  /**< contiendra la chaîne à hasher */
 
-    sha256ofString((BYTE *)item,hashRes); // hashRes contient maintenant le hash du code avec le nom , prenom et code
-    /// \param[out] on récupere le hashRes qui contient le hash de nom , prenom et code
+    sprintf(stringtohash,"%s %s%s",nom,prenom,code);
 
+    sha256ofString((BYTE *)stringtohash, hashresult); /**< hashresult contient maintenant le hash du code avec le nom , prenom et code */
+    /// \param[out] hashresult le résultat qui contient le hash de nom , prenom et code
 
-    t_mat_char_star_dyn *entete ;
+    t_mat_char_star_dyn *entete;
     t_mat_char_star_dyn *result;
-    /* On appelle la fonction du lecteur_csv  */
-    /// \brief appelle du lecteur csv , affichage de l'entete et la correspondance du hash et fin de main
+    
+    /// \brief appel du lecteur csv , affichage de l'entete et la correspondance du hash et fin de main
     entete = lecture_entete(nom_csv);
-    /// \param[out] Récuperation de l'entete
-    result =recherche_hash(hashRes, nom_csv);
-    /// \param[out] récuperation de la ligne du hash 
-    affichage_resultat(entete ,result );
-    /// \param[out] affichage des résultats 
-    // a partir de la colonne 4 , et ligne 0 et ligne que l'on veut 
-    free(item);
-
+    /// \param[out] entête entête du CSV lu
+    result = recherche_hash(hashresult, nom_csv);
+    /// \param[out] result ligne du CSV contenant hashresult si elle existe
+    affichage_resultat(entete, result);
+    /// \param[out] affichage des résultats uniquement la colonne 4 de l'entête et de la ligne de vote recherchée
+    free(stringtohash);
+    return 0;
 }
