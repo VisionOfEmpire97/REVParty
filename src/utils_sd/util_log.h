a8ae65b70586a6415c7a9c9b660d98bb7e47d6c6
Nathan ROSET <nathroset97@gmail.com>
2023-11-07 02:05:09 +0100

    formatage des commentaires

--- src/utils_sd/util_log.h
+++ src/utils_sd/util_log.h
@@ -11,16 +11,20 @@
  * pour concaténer l'étape de calcul au fichier filename 
  * passé en paramètre et déjà ouvert en écriture
  * PROPOSITION pour format de l'étape de calcul : 
+ * *****************************************
+ * 
  * **début du programme [nom du programme]**
  * [nom du prgm] - étape réalisée 
  * **fin du programme [nom du programme]**
+ * 
+ * *****************************************
  * Le log sera fermé dans le main principal
  * 
  * utiliser dup ? à voir...
  * 
  * @param filename le fichier de log déjà ouvert
  */
-void to_log_file(char* filename);
+void append_to_log_file(char* filename);
 
 /**
  * @brief Affiche les logs recueillis tout au long 
