c674ea439b1ac5032a3917fffa5176a7aeba615a
NpiePrime <yanis.yzr@gmail.com>
2023-11-22 20:28:22 +0100

    condorcet minimax

---
+++ src/condorcet.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+// #include <condorcet.h>
+#include "utils_sd/graph.h"
+
+sommet *vainqueurCondorcet(graph *graph)
+{
+
+    sommet *vainqueur = NULL;
+    for (unsigned i = 0; i < graph->nbSommet; i++)
+    {
+        sommet *s = graph->sommets[i];
+        if (s->nbSuccesseur == (graph->nbSommet - 1))
+            vainqueur = s;
+    }
+
+    /*TODO : Print logs*/
+
+    return vainqueur;
+}
+
+sommet *vainqueurCondorcetMinimax(graph *graph){
+
+    int maxmin = 0;
+    sommet *s, *t, *res;
+    arc* a = graph->arcs;
+    for (unsigned i = 0; i < graph->nbSommet; i++)
+    {
+        s = graph->sommets[i];
+        a->depart = s;
+        for (unsigned j = 0; j < (graph->nbSommet -1); j++)
+        {  
+            t = graph->sommets[j]; 
+            a->arrivee = j;
+            if(a->poids < maxmin)
+            {
+                maxmin = a->poids;
+                res = s;
+            }
+            
+        }
+        
+    }
+    /*TODO : Print logs*/
+    return res;
+
+}
---
+++ src/condorcet.h
@@ -0,0 +1,19 @@
+/**
+ * \author Équipe 001 (ZERKANI Yanis)
+ * \date 20 Novembre 2023
+ * \brief Programme permettant de lire un fichier CSV (Comma Separated Value) et de renvoyer son contenu sous forme de matrice.
+ * La matrice est de type t_mat_char_star_dyn.
+ * \file lecture_csv.h
+ */
+
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "utils_sd/graph.h"
+
+/**
+ * 
+ * 
+*/
+sommet *vainqueurCondorcet(graph *graph);
