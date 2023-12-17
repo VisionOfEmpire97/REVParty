7cc57d8e93ff87ef4974fe5ce91cc2385f9dc148
Nathan ROSET <nathroset97@gmail.com>
2023-12-17 21:10:59 +0100

    Correction de l'emploi de la balise -d
    On peut maintenant employer la balise -d avec les méthodes de condorcet

--- src/scrutin.c
+++ src/scrutin.c
@@ -24,7 +24,7 @@
  * Analyse des balises et arguments passés en paramètre et appel des
  * différentes méthodes de scrutins
  * \{
-*/
+ */
 #include <stdio.h>
 #include <getopt.h>
 #include <stdlib.h>
@@ -69,23 +69,23 @@ int main(int argc, char **argv)
             nom_log = optarg;
             n++;
             break;
-        case 'm': // ne prend qu'un argument 
+        case 'm': // ne prend qu'un argument
             methode = optarg;
             n++;
             break;
         default:
-            printf("%susage : ./scrutin [-i|d nom_du_csv] [-o nom_du_log] [-m méthodes]%s\n",RED,END_COLOR);
+            printf("%susage : ./scrutin [-i|d nom_du_csv] [-o nom_du_log] [-m méthodes]%s\n", RED, END_COLOR);
             exit(EXIT_FAILURE);
         }
     }
     if (nom_csv == NULL || methode == NULL)
     {
-        printf("%susage : ./scrutin [-i|d nom_du_csv] [-o nom_du_log] [-m méthodes]%s\n",RED,END_COLOR);
+        printf("%susage : ./scrutin [-i|d nom_du_csv] [-o nom_du_log] [-m méthodes]%s\n", RED, END_COLOR);
         exit(EXIT_FAILURE);
     }
-    if (type_csv == 'd' && (strcmp(methode, "uni1") || strcmp(methode, "uni2") || strcmp(methode, "jm")))
+    if (type_csv == 'd' && (!strcmp(methode, "uni1") || !strcmp(methode, "uni2") || !strcmp(methode, "jm")))
     {
-        printf("%sAttention, la balise -d désactive l'emploi des arguments uni1,uni2 et jm%s\n",RED,END_COLOR);
+        printf("%sAttention, la balise -d désactive l'emploi des arguments uni1,uni2 et jm%s\n", RED, END_COLOR);
         exit(EXIT_FAILURE);
     };
 
@@ -94,7 +94,7 @@ int main(int argc, char **argv)
     lancer_methode(methode, nom_csv, &type_csv);
 
     close_log_file();
-    
+
     return 0;
 }
 
